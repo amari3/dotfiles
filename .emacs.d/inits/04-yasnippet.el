05895424d49208063cff428ec017f78a221606e4
amari3 <ryo2.amari3@gmail.com>
2012-09-30 16:42:28 +0900

    yasnippet用の設定を色々貼りつけた。不要な箇所は後で削除する

--- .emacs.d/inits/04-yasnippet.el
+++ .emacs.d/inits/04-yasnippet.el
@@ -1,6 +1,29 @@
 ;;; Yasnippet
-;; (auto-install-from-url "https://raw.github.com/capitaomorte/yasnippet/master/yasnippet.el")
+;; (package-install 'yasnippet)
 (require 'yasnippet)
-(setq yas/snippet-dirs '("~/.emacs.d/inits/etc/snippets"))
+(custom-set-variables
+ '(yas/trigger-key  "M-o")
+ '(yas/snippet-dirs '("~/.emacs.d/etc/snippets")))
 (yas/global-mode t)
 
+;; anything interface
+(eval-after-load "anything-config"
+  '(progn
+     (defun my-yas/prompt (prompt choices &optional display-fn)
+       (let* ((names (loop for choice in choices
+                           collect (or (and display-fn (funcall display-fn choice))
+                                       choice)))
+              (selected (anything-other-buffer
+                         `(((name . ,(format "%s" prompt))
+                            (candidates . names)
+                            (action . (("Insert snippet" . (lambda (arg) arg))))))
+                         "*anything yas/prompt*")))
+         (if selected
+             (let ((n (position selected names :test 'equal)))
+               (nth n choices))
+           (signal 'quit "user quit!"))))
+     (custom-set-variables '(yas/prompt-functions '(my-yas/prompt)))
+     (define-key anything-command-map (kbd "y") 'yas/insert-snippet)))
+
+;; snippet-mode for *.yasnippet files
+;(add-to-list 'auto-mode-alist '("\\.yasnippet$" . snippet-mode))
