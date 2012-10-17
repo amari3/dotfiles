c431231318dce0271547cbf1fc8f7966a7c7a20e
amari3 <ryo2.amari3@gmail.com>
2012-10-17 22:05:42 +0900

    CUIの時にエラーになっていたのを修正

--- .emacs.d/inits/02-font.el
+++ .emacs.d/inits/02-font.el
@@ -1,9 +1,8 @@
 ;;; font
-
-(set-default-font "Inconsolata-11")
-(set-face-font 'variable-pitch "Inconsolata-11")
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("Takaoゴシック" . "unicode-bmp")
-)
-
+(add-to-list 'default-frame-alist '(font . "Inconsolata-12"))
+(cond (window-system
+       (set-default-font "Inconsolata-12")
+       (set-fontset-font (frame-parameter nil 'font)
+                          'japanese-jisx0208
+                           '("Takaoゴシック" . "unicode-bmp"))
+       ))
