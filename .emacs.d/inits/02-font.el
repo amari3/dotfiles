a94fd587af9fc811c40a39c40b7e6124f1f60f88
amari3 <ryo2.amari3@gmail.com>
2012-10-17 23:03:03 +0900

    フォントサイズがでかかったので調整

--- .emacs.d/inits/02-font.el
+++ .emacs.d/inits/02-font.el
@@ -1,7 +1,7 @@
 ;;; font
-(add-to-list 'default-frame-alist '(font . "Inconsolata-12"))
+(add-to-list 'default-frame-alist '(font . "Inconsolata-11"))
 (cond (window-system
-       (set-default-font "Inconsolata-12")
+       (set-default-font "Inconsolata-11")
        (set-fontset-font (frame-parameter nil 'font)
                           'japanese-jisx0208
                            '("Takaoゴシック" . "unicode-bmp"))
