d3a9a0b9c3e33c4333a6dc7442639eb3c3a25750
amari3 <ryo2.amari3@gmail.com>
2012-10-22 11:20:48 +0900

    markdown-modeの設定をシンプルにした

--- .emacs.d/inits/26-markdown.el
+++ .emacs.d/inits/26-markdown.el
@@ -2,7 +2,5 @@
 ;; Mac: brew install markdown
 ;; Linux: aptitude install markdown
 ;; (auto-install-from-url "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
-(autoload 'markdown-mode "markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-(setq auto-mode-alist
-      (cons '("\\.md" . markdown-mode) auto-mode-alist))
+(autoload 'markdown-mode "markdown-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
