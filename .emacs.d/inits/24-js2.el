5590ce087ccfef3e9bd7f2049dfa2fe88166fa3d
amari3 <ryo2.amari3@gmail.com>
2012-10-22 12:03:56 +0900

    js2-mode用のキーバンドを追加

--- .emacs.d/inits/24-js2.el
+++ .emacs.d/inits/24-js2.el
@@ -1,5 +1,20 @@
 ;; js2-mode
 ;; (auto-install-from-url "https://raw.github.com/mooz/js2-mode/master/js2-mode.el")
 (autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.\\(js\\|json\\)$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
+(eval-after-load "js2-mode"
+  '(progn
+     ;; key config
+     (define-key js2-mode-map (kbd "C-m") 'newline-and-indent)
+     (define-key js2-mode-map (kbd "M-n") 'next-error)
+     (define-key js2-mode-map (kbd "M-p") 'previous-error)))
+
+;; hook
+(defun my-js2-mode-hook ()
+  (when (boundp 'auto-complete-mode)
+    (eval
+     '(add-to-list 'ac-sources 'ac-source-dictionary)))
+  (set-face-foreground 'js2-function-param-face "SeaGreen3"))
+(add-hook 'js2-mode-hook 'my-js2-mode-hook)
