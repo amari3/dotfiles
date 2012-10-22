f19ce2d45dd929f2b121b217ca05393883762fda
amari3 <ryo2.amari3@gmail.com>
2012-10-22 12:02:48 +0900

    requireからautoloadへ変更した

--- .emacs.d/inits/25-yaml.el
+++ .emacs.d/inits/25-yaml.el
@@ -1,4 +1,4 @@
 ;; yaml-mode
 ;; (auto-install-from-url "https://raw.github.com/yoshiki/yaml-mode/master/yaml-mode.el")
-(when (require 'yaml-mode nil t)
-  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+(autoload 'yaml-mode "yaml-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
