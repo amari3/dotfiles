d0fd0abde8253aa83c3f93ddf210d5ec6b9ce77e
amari3 <ryo2.amari3@gmail.com>
2012-10-22 15:05:19 +0900

    auto-completeの設定を追加

--- .emacs.d/inits/03-editing.el
+++ .emacs.d/inits/03-editing.el
@@ -11,3 +11,13 @@
 ;; show parentheses
 (show-paren-mode t)
 
+;; auto-complete
+;; (auto-install-batch "auto-complete development version")
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+(custom-set-variables
+  '(ac-use-menu-map t)
+   '(ac-dictionary-directories '("~/.emacs.d/etc/ac-dict")))
+(define-key ac-menu-map (kbd "C-n") 'ac-next)
+(define-key ac-menu-map (kbd "C-p") 'ac-previous)
+(define-key ac-menu-map (kbd "M-TAB") nil)
