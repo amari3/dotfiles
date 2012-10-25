971e4b5c9472cf25abe4e60626c4a945d9beb230
amari3 <ryo2.amari3@gmail.com>
2012-10-25 16:08:34 +0900

    閉じ括弧が多く記述されていたので修正

--- .emacs.d/inits/25-yaml.el
+++ .emacs.d/inits/25-yaml.el
@@ -1,4 +1,4 @@
 ;; yaml-mode
 ;; (auto-install-from-url "https://raw.github.com/yoshiki/yaml-mode/master/yaml-mode.el")
 (autoload 'yaml-mode "yaml-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode)))
+(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
