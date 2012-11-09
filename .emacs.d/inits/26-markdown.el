a2d5108c80f6df6ef51fbe5f5fe7c7aac1cd7bbb
amari3 <ryo2.amari3@gmail.com>
2012-11-09 11:17:39 +0900

    コメントを修正しただけ

--- .emacs.d/inits/26-markdown.el
+++ .emacs.d/inits/26-markdown.el
@@ -1,6 +1,9 @@
+;;; Markdown
+
 ;; markdown-mode
-;; Mac: brew install markdown
-;; Linux: aptitude install markdown
 ;; (auto-install-from-url "http://jblevins.org/projects/markdown-mode/markdown-mode.el")
+;; to install markdown command
+;; Mac: brew install markdown
+;; Linux (deb): aptitude install markdown
 (autoload 'markdown-mode "markdown-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
