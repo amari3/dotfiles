53f5b9159e159d935422ab797e5cb5fae59b7c10
amari3 <ryo2.amari3@gmail.com>
2012-11-27 14:38:17 +0900

    CSSの設定を追加

---
+++ .emacs.d/inits/27-css.el
@@ -0,0 +1,16 @@
+;;; CSS
+
+;; auto-complete
+(eval-when-compile
+    (require 'cl)
+      (require 'auto-complete-config))
+(defvar ac-source-css-property-names
+    '((candidates . (loop for property in ac-css-property-alist
+                                                  collect (car property)))))
+(defun my-css-mode-hook ()
+    (add-to-list 'ac-sources 'ac-source-css-property)
+      (add-to-list 'ac-sources 'ac-source-css-property-names))
+(add-hook 'css-mode-hook 'my-css-mode-hook)
+
+(setq cssm-indent-level 4)
+(setq cssm-indent-function #'cssm-c-style-indenter)
