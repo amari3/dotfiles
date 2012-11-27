748566a23bee19d218c6b324d87c247f594bc31f
amari3 <ryo2.amari3@gmail.com>
2012-11-27 14:40:12 +0900

    インデント修正

--- .emacs.d/inits/27-css.el
+++ .emacs.d/inits/27-css.el
@@ -2,14 +2,14 @@
 
 ;; auto-complete
 (eval-when-compile
-    (require 'cl)
-      (require 'auto-complete-config))
+  (require 'cl)
+  (require 'auto-complete-config))
 (defvar ac-source-css-property-names
-    '((candidates . (loop for property in ac-css-property-alist
-                                                  collect (car property)))))
+  '((candidates . (loop for property in ac-css-property-alist
+                        collect (car property)))))
 (defun my-css-mode-hook ()
-    (add-to-list 'ac-sources 'ac-source-css-property)
-      (add-to-list 'ac-sources 'ac-source-css-property-names))
+  (add-to-list 'ac-sources 'ac-source-css-property)
+  (add-to-list 'ac-sources 'ac-source-css-property-names))
 (add-hook 'css-mode-hook 'my-css-mode-hook)
 
 (setq cssm-indent-level 4)
