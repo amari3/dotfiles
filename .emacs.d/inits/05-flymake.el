aa75b3cf75c4a255c736f7245246ae6f7e5430b7
amari3 <ryo2.amari3@gmail.com>
2013-04-17 17:28:06 +0900

    flymakeのエラー行表示色を変更

--- .emacs.d/inits/05-flymake.el
+++ .emacs.d/inits/05-flymake.el
@@ -7,3 +7,5 @@
 (global-set-key "\M-n" 'flymake-goto-next-error)
 
 (global-set-key "\C-cd" 'flymake-display-err-menu-for-current-line)
+(set-face-background 'flymake-errline "red3")
+(set-face-background 'flymake-warnline "orange3")
