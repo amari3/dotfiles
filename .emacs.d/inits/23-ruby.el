c1da5625a9e39891770a03255a033ac4570159cc
amari3 <ryo2.amari3@gmail.com>
2014-01-12 01:13:34 +0900

    add ruby-mode setting

---
+++ .emacs.d/inits/23-ruby.el
@@ -0,0 +1,15 @@
+;;; Ruby
+
+;; ruby-mode
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (setq tab-width 2)
+             (setq ruby-indent-level tab-width)
+             (setq ruby-deep-indent-paren-style nil)
+             (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)))
