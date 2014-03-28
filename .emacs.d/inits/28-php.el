fef6216fc5988e43772f15fa7444993ca9c06067
amari3 <ryo2.amari3@gmail.com>
2014-03-29 00:31:57 +0900

    インデントをいい感じに対応

--- .emacs.d/inits/28-php.el
+++ .emacs.d/inits/28-php.el
@@ -6,3 +6,16 @@
 
 (setq php-mode-force-pear t)
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+
+(add-hook 'php-mode-hook
+          (lambda ()
+            (defun ywb-php-lineup-arglist-intro (langelem)
+              (save-excursion
+                (goto-char (cdr langelem))
+                (vector (+ (current-column) c-basic-offset))))
+            (defun ywb-php-lineup-arglist-close (langelem)
+              (save-excursion
+                (goto-char (cdr langelem))
+                (vector (current-column))))
+            (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+            (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
