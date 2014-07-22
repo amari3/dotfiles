17794f38a425ab3aabbf7624ceb69975bfba1f4d
amari3 <ryo2.amari3@gmail.com>
2014-07-22 15:57:47 +0900

    sr-speedbarに表示する拡張子の設定を追加

--- .emacs.d/inits/06-sr-speedbar.el
+++ .emacs.d/inits/06-sr-speedbar.el
@@ -4,3 +4,7 @@
 (require 'sr-speedbar)
 (setq sr-speedbar-right-side nil)
 (global-set-key (kbd "C-^") 'sr-speedbar-toggle)
+
+(add-hook 'speedbar-mode-hook
+          '(lambda ()
+             (speedbar-add-supported-extension '("js" "html" "css" "php" "tpl" "ctp" "*"))))
