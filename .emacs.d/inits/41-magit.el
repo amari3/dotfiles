63d828cff8a971cab84cf8b3424557569e44e558
amari3 <ryo2.amari3@gmail.com>
2014-07-30 17:00:07 +0900

    magitの設定を追加

---
+++ .emacs.d/inits/41-magit.el
@@ -0,0 +1,10 @@
+;;; Magit
+;; (package-install 'magit)
+
+;; color
+(eval-after-load 'magit
+  '(progn
+     (set-face-background 'magit-item-highlight "#202020")
+     (set-face-foreground 'magit-diff-add "#40ff40")
+     (set-face-foreground 'magit-diff-del "#ff4040")
+     (set-face-foreground 'magit-diff-file-header "#4040ff")))
