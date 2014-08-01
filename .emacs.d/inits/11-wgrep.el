ebea0c31a2a6816c4c7f211129b7f647189d2885
amari3 <ryo2.amari3@gmail.com>
2014-08-01 14:34:08 +0900

    wgrepでagを利用するようにする

--- .emacs.d/inits/11-wgrep.el
+++ .emacs.d/inits/11-wgrep.el
@@ -1,5 +1,13 @@
 ;;; wgrep
-;; (auto-install-from-emacswiki "wgrep.el")
-
-(require 'wgrep nil t)
-
+;; (package-install 'ag)
+;; (package-install 'wgrep)
+;; (package-install 'wgrep-ag)
+(require 'ag)
+(custom-set-variables
+ '(ag-highlight-search t)
+ '(ag-reuse-window 'nil)
+ '(ag-reuse-buffers 'nil))
+(require 'wgrep-ag)
+(autoload 'wgrep-ag-setup "wgrep-ag")
+(add-hook 'ag-mode-hook 'wgrep-ag-setup)
+(define-key ag-mode-map (kbd "r") 'wgrep-change-to-wgrep-mode)
