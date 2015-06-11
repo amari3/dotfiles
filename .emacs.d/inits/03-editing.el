929d3bd3d9bed3b6dee926bfe22a5d048529a505
amari3 <ryo2.amari3@gmail.com>
2015-06-11 23:46:13 +0900

    EmacsとOSXのクリップボード共有設定を追加

--- .emacs.d/inits/03-editing.el
+++ .emacs.d/inits/03-editing.el
@@ -24,4 +24,17 @@
 
 ;; scroll
 (setq scroll-step 1)
-(setq-scroll-bar-mode 'right)
+(set-scroll-bar-mode 'right)
+
+;; copy/paste
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
